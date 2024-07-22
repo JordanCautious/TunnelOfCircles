2d709cf4e76742b542792f781b8fbbd65523e5b2
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-07-21 21:10:11 -0400

    Initial Commit

---
+++ TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunnelOfCirclesApp.swift
+//  TunnelOfCircles
+//
+//  Created by Jordan Haynes on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TunnelOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
