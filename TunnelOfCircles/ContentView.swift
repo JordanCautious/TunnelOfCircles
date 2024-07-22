9f65098004d2d03ec1f5eac145676e0ac05ce69a
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-07-21 21:43:10 -0400

    Complete the app

--- TunnelOfCircles/ContentView.swift
+++ TunnelOfCircles/ContentView.swift
@@ -6,18 +6,51 @@
 //
 
 import SwiftUI
-import RealityKit
-import RealityKitContent
 
 struct ContentView: View {
+    @State private var colors: [Color] = [.cyan,.blue]
+    @State private var depth = 0.0
+    
+    let minDiameter = 50.0
+    let diameterChange = 70.0
+    
     var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            Text("Tunnel of Circles")
+                .font(.largeTitle)
+                .padding()
+            ZStack {
+                ForEach(0..<4) { index in
+                    Circle()
+                        .stroke(lineWidth: 30)
+                        .foregroundStyle(colors[index % 2])
+                        .frame(height: minDiameter + diameterChange * Double(index))
+                        .padding3D(.back, depth)
+                }
+            }
+            .padding(.bottom, 30)
+            Grid {
+                GridRow {
+                    Text("Depth")
+                    Slider(value: $depth, in: 0...50) {
+                        Text("Depth")
+                    }
+                }
+                GridRow {
+                    Text("Colors")
+                    HStack {
+                        ColorPicker("Color", selection: $colors[0])
+                        ColorPicker("Color", selection: $colors[1])
+                        Spacer()
+                    }
+                    .labelsHidden()
+                }
+            }
+            .padding(30)
+            .background(.thickMaterial)
+            .frame(minWidth: 320)
         }
-        .padding()
+        .frame(minHeight: 560)
     }
 }
 
--- TunnelOfCircles/TunnelOfCirclesApp.swift
+++ TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -13,5 +13,6 @@ struct TunnelOfCirclesApp: App {
         WindowGroup {
             ContentView()
         }
+        .windowResizability(.contentSize)
     }
 }
